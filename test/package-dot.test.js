f0d808fd87af8cbd2ba480e3c39f3f4a385d0ea3
Derek Shoemaker <shoemakerdr@gmail.com>
2017-10-02 21:28:49 +0000

    Added tests

--- test/package-dot.test.js
+++ test/package-dot.test.js
@@ -14,12 +14,67 @@ describe('PackageDot', function () {
 describe('#add', function () {
     const json = JSON.stringify({a:1,b:2,c:3})
     const pd = new PackageDot(json)
-    describe('when given a key-value pair', function () {
+    
+    it('returns itself so it can be chained', function () {
+        const added = {c:3}
+        pd.add(added)
+        assert.isTrue(pd.add(added) instanceof PackageDot)
+    })
+    
+    it('will add a given object to the package', function () {
         const added = {d:4}
         const withAdded = {a:1,b:2,c:3,d:4}
         pd.add(added)
-        it('will add the key and value in the package object', function () {
-            assert.deepEqual(pd.getPackage(), withAdded)
-        })
+        assert.deepEqual(pd.getPackage(), withAdded)
+    })
+    
+    it('will overwrite an existing key if existing key is present in the arg object', function () {
+        const added = {c:4}
+        const withAdded = {a:1,b:2,c:4, d:4}
+        pd.add(added)
+        assert.deepEqual(pd.getPackage(), withAdded)
+    })
+})
+
+describe('#pushTo', function () {
+    const json = JSON.stringify({a:1,b:2,c:[1,2]})
+    const pd = new PackageDot(json)
+    
+    it('returns itself so it can be chained', function () {
+        const pushed = 3
+        assert.isTrue(pd.pushTo(pushed, 'c') instanceof PackageDot)
+    })
+    
+    it('will append a given value to an array in a given key', function () {
+        const pushed = 4
+        const withPushed = {a:1,b:2,c:[1,2,3,4]}
+        pd.pushTo(pushed, 'c')
+        assert.deepEqual(pd.getPackage(), withPushed)
+    })
+    
+    it('will throw error when given a key that does not exist', function () {
+        const pushed = 1
+        assert.throws(() => pd.pushTo(pushed, 'd'), 'Cannot push to key that does not exist')
+    })
+    
+    it('will throw error when given entry on object is not an array', function () {
+        const pushed = 1
+        const push = () => pd.pushTo(pushed, 'a')
+        assert.throws(push, 'Cannot push value to non-array')
+    })
+})
+
+describe('#addTo', function () {
+    const json = JSON.stringify({a:1,b:{a:1}})
+    const pd = new PackageDot(json)
+    
+    it('returns itself so it can be chained', function () {
+        assert.isTrue(pd.addTo({b:2}, 'b') instanceof PackageDot)
+    })
+    
+    it('will add an entry to a given target key', function () {
+        const withAdded = {a:1,b:{a:1,b:2,c:3}}
+        pd.addTo({c:3}, 'b')
+        assert.deepEqual(pd.getPackage(), withAdded)
     })
 })
