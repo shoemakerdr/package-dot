40bee6868cd5797edc09131b8d839011fc193eed
Derek Shoemaker <shoemakerdr@gmail.com>
2017-10-02 21:28:39 +0000

    Added addTo and pushTo methods

--- src/package-dot.js
+++ src/package-dot.js
@@ -7,16 +7,33 @@ class PackageDot {
         this.toJSON = this.toJSON.bind(this)
         this.pkg = JSON.parse(pkg)
     }
-    
+
     getPackage () {
         return this.pkg
     }
-    
+
     add (obj) {
         this.pkg = Object.assign({}, this.pkg, obj)
         return this
     }
-    
+
+    addTo (obj, target) {
+        if (!this.pkg[target]) {
+            throw new Error('Cannot add to key that does not exist')
+        }
+        this.pkg[target] = Object.assign({}, this.pkg[target], obj)
+        return this
+    }
+
+    pushTo (entry, target) {
+        if (!this.pkg[target])
+            throw new Error('Cannot push to key that does not exist')
+        if (!Array.isArray(this.pkg[target]))
+            throw new Error('Cannot push value to non-array')
+        this.pkg[target].push(entry)
+        return this
+    }
+
     update (key, value) {
         if (!this.pkg[key]) {
             throw new Error('Cannot update key that does not exist')
@@ -24,12 +41,12 @@ class PackageDot {
         this.pkg = Object.assign({}, {[key]:value}, this.pkg)
         return this
     }
-    
+
     remove (key) {
         delete this.pkg[key]
         return this
     }
-    
+
     toJSON () {
         return JSON.stringify(this.pkg)
     }
