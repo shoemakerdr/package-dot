a1b48d9c745358fe17b4c7545d9f1cd0b1248338
Derek Shoemaker <shoemakerdr@gmail.com>
2017-10-02 23:14:32 -0500

    commented out

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import readFilePromise from '../utils/read-file-promise'
-import writeFilePromise from '../utils/write-file-promise'
-import path from 'path'
-import PackageDot from './package-dot'
+// import readFilePromise from '../utils/read-file-promise'
+// import writeFilePromise from '../utils/write-file-promise'
+// import path from 'path'
+// import PackageDot from './package-dot'
 
 
 
