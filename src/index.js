a99a001af556cada209fb2a725da1b2c8b0ca394
Derek Shoemaker <shoemakerdr@gmail.com>
2017-10-06 17:34:07 +0000

    Added commander dep

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// import readFilePromise from '../utils/read-file-promise'
-// import writeFilePromise from '../utils/write-file-promise'
-// import path from 'path'
-// import PackageDot from './package-dot'
-
-
-
-// export default packageDot
