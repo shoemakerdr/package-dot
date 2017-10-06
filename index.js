c5ffd5e66583894fbd3cd3bd324e5eb2d87c2320
Derek Shoemaker <shoemakerdr@gmail.com>
2017-10-06 17:33:37 +0000

    Changed src/index to src/package-dot

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // enable @std/esm loader
 require = require('@std/esm')(module, { cjs: true, esm: "js" })
-module.exports = require('./src/index.js').default
+module.exports = require('./src/package-dot.js').default
